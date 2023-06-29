7ab17f928432cbe23dfa47cbe1d5ee7030f33cb5
mdowns <michaeljdowns99@gmail.com>
2023-06-29 14:56:22 -0600

    Started Final Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { CompletedComponent } from './completed/completed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ' ', redirectTo: '/todoList', pathMatch: 'full' },
+  {path: 'todoList', component: TodoComponent},
+  {path: 'completedItems', component: CompletedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DropdownDirectiveDirective } from './directive/dropdown-directive';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
+import { TodoEditComponent } from './todo/todo-edit/todo-edit.component';
+import { CompletedComponent } from './completed/completed.component';
+import { CompletedListComponent } from './completed/completed-list/completed-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DropdownDirectiveDirective,
+    TodoComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoDetailComponent,
+    TodoEditComponent,
+    CompletedComponent,
+    CompletedListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
